8a2b071a0cee1eee9ed87a1a9c2b0baf842e2036
Jonas Abde <empire1266@gmail.com>
2023-10-12 22:33:25 +0200

    Final version, v2
    Cookie clikker spiller er færdigt og klar til at blive afspillet i morgen.
    
    tutorial:
    Cookie Clicker er et simpelt spil, hvor spilleren skal klikke på en cookie for at tjene point. Jo flere point spilleren tjener, jo flere cookies kan spilleren købe, som automatisk genererer point. Spillet er kendt for sin afhængighedsskabende natur og simple gameplay.
    
    Her er en guide til at spille Cookie Clicker:
    
    Åbn spillet i din browser.
    Klik på cookien for at tjene point.
    Brug pointene til at købe cookies, som automatisk genererer point.
    Brug pointene til at købe opgraderinger, som øger pointgenereringen.
    Gentag trin 2-4 for at tjene flere point og købe flere cookies og opgraderinger.
    Hold øje med specielle begivenheder og præmier, som kan hjælpe dig med at tjene flere point.
    Fortsæt med at spille og se, hvor mange point du kan tjene!

--- app.js
+++ app.js
@@ -1,190 +1,213 @@
-const cookie = document.querySelector("#cookie");
-const autoClick = document.querySelector("#auto-click");
-const autoClickTextPrice = document.querySelector("#auto-click .price span");
-const upgradeClick = document.querySelector("#upgrade-click");
-const upgradeClickTextPrice = document.querySelector("#upgrade-click .price span");
 
-const updateScore = cookies => {
-    const title = document.querySelector("title");
-    const score = document.querySelector("#score span");
+// for at se koden til de forskellige powerups, så se i app.js, linje 1-100 og linje 100-200 og linje 200-300. samt i index.html, linje 1-100 og linje 100-200 og linje 200-300.
+const cookie = document.querySelector("#cookie"); /* viser antal cookie */
+const autoClick = document.querySelector("#auto-click"); /* viser antal auto-clicks */
+const autoClickTextPrice = document.querySelector("#auto-click .price span"); /* viser prisen på auto-clicks */
+const upgradeClick = document.querySelector("#upgrade-click"); /*  viser antal upgrade-clicks */
+const upgradeClickTextPrice = document.querySelector("#upgrade-click .price span"); /* viser prisen på upgrade-clicks */
 
-    score.innerText = cookies;
-    title.innerHTML = cookies + " cookies - Cookie Clicker"
 
-    localStorage.setItem("cookies", cookies);
+// Path: app.js
+const updateScore = cookies => { /* opdaterer antal cookies */
+    const title = document.querySelector("title"); /* opdaterer titlen på siden */
+    const score = document.querySelector("#score span"); /* opdaterer antal cookies */
+
+    score.innerText = cookies; // opdaterer antal cookies
+    title.innerHTML = cookies + " cookies - Cookie Clicker" // opdaterer titlen på siden
+
+    localStorage.setItem("cookies", cookies); // gemmer antal cookies i localstorage
 }
 
-const updatePowerupsStorage = powerup => {
-    let powerups = JSON.parse(localStorage.getItem("powerups")) || [];
-    powerups.push(powerup);
+// updateScore(0); // opdaterer antal cookies
+const updatePowerupsStorage = powerup => { /* opdaterer antal powerups */
+    let powerups = JSON.parse(localStorage.getItem("powerups")) || []; // henter powerups fra localstorage
+    powerups.push(powerup);     // tilføjer powerup til array
 
-    localStorage.setItem("powerups", JSON.stringify(powerups));
+    localStorage.setItem("powerups", JSON.stringify(powerups)); // gemmer powerups i localstorage
 }
 
-const getStorage = () => {
-    const cookies = localStorage.getItem("cookies") || 0;
-    const powerups = JSON.parse(localStorage.getItem("powerups")) || [];
-
-    const storage = {
-        "cookies": cookies,
-        "powerups": powerups
+// updatePowerupsStorage("auto-click"); // opdaterer antal powerups
+const getStorage = () => {  /* henter antal cookies og powerups fra localstorage */
+    const cookies = localStorage.getItem("cookies") || 0; // henter antal cookies fra localstorage
+    const powerups = JSON.parse(localStorage.getItem("powerups")) || [];    // henter powerups fra localstorage
+ 
+    const storage = {  // opretter et objekt med antal cookies og powerups
+        "cookies": cookies, // antal cookies
+        "powerups": powerups // powerups
     }
 
-    return storage;
+    return storage; // returnerer objektet
 }
 
-const cookieClicked = cookies => {
-    const storage = getStorage();
+// getStorage(); // henter antal cookies og powerups fra localstorage
+const cookieClicked = cookies => { /* opdaterer antal cookies */
+    const storage = getStorage(); // henter antal cookies og powerups fra localstorage
 
-    const score = document.querySelector("#score span");
-    const scoreValue = cookies ? cookies : parseInt(score.innerText);
+    const score = document.querySelector("#score span"); // henter antal cookies fra html
+    const scoreValue = cookies ? cookies : parseInt(score.innerText); // hvis cookies er defineret, så brug den værdi, ellers hent antal cookies fra html
 
-    let newScore;
+    let newScore; // opretter variabel til at gemme antal cookies
 
-    if(storage.powerups.includes("upgrade-click")) {
-        const multiplier = storage.powerups.filter(powerup => powerup == "upgrade-click").length;
-        if(multiplier == 1){
-            newScore = scoreValue + 2;
-        } else {
-            newScore = scoreValue + (2 ** multiplier)
+    if(storage.powerups.includes("upgrade-click")) { // hvis der er en upgrade-click powerup
+        const multiplier = storage.powerups.filter(powerup => powerup == "upgrade-click").length; // henter antal upgrade-clicks
+        if(multiplier == 1){    // hvis der er 1 upgrade-click
+            newScore = scoreValue + 2; // tilføj 2 cookies
+        } else {   // hvis der er flere upgrade-clicks
+            newScore = scoreValue + (2 ** multiplier) // tilføj 2^multiplier cookies
         }
-    } else {
-        newScore = scoreValue + 1;
+    } else {   // hvis der ikke er en upgrade-click powerup
+        newScore = scoreValue + 1; // tilføj 1 cookie
     }
 
-    updateScore(newScore);
+    updateScore(newScore); // opdaterer antal cookies
 }
 
-const createParticle = (x,y) => {
-    const cookieClicks = document.querySelector(".cookie-clicks");
 
-    const particle = document.createElement("img");
-    particle.setAttribute("src", "img/cookie.png");
-    particle.setAttribute("class", "cookie-particle");
-    particle.style.left = x + "px";
-    particle.style.top = y + "px";
+// cookie.addEventListener("click", cookieClicked); // tilføjer eventlistener til cookie
+const createParticle = (x,y) => { /* opretter en cookie particle */
+    const cookieClicks = document.querySelector(".cookie-clicks"); // henter cookie-clicks div
 
-    cookieClicks.appendChild(particle);
+    const particle = document.createElement("img"); // opretter et img element
+    particle.setAttribute("src", "img/cookie.png"); // sætter src på img elementet
+    particle.setAttribute("class", "cookie-particle"); // sætter class på img elementet
+    particle.style.left = x + "px"; // sætter x position på img elementet
+    particle.style.top = y + "px"; // sætter y position på img elementet
+
+    cookieClicks.appendChild(particle); // tilføjer img elementet til cookie-clicks div
 
 
-    setTimeout(() => {
-        cookieClicks.removeChild(particle);
-    }, 3000);
+    setTimeout(() => {  // fjerner img elementet efter 3 sekunder
+        cookieClicks.removeChild(particle); // fjerner img elementet fra cookie-clicks div
+    }, 3000);   // 3000ms = 3 sekunder
 }
 
-cookie.addEventListener("click", (e) => {
-    createParticle(e.clientX, e.clientY);
-    cookieClicked()
+// createParticle(100, 100); // opretter en cookie particle
+cookie.addEventListener("click", (e) => { // tilføjer eventlistener til cookie
+    createParticle(e.clientX, e.clientY); // opretter en cookie particle
+    cookieClicked() // opdaterer antal cookies
 });
 
-const autoClickCookie = () => {
-    setInterval(() => {
-        const score = document.querySelector("#score span");
-        const scoreValue = parseInt(score.innerText);
 
-        newScore = scoreValue + 1;
+// Path: app.js
+const autoClickCookie = () => { /* opretter en cookie particle */
+    setInterval(() => { // opretter en cookie particle hvert sekund
+        const score = document.querySelector("#score span");    // henter antal cookies fra html
+        const scoreValue = parseInt(score.innerText);  // henter antal cookies fra html
 
-        updateScore(newScore);
-    }, 1000)
+        newScore = scoreValue + 1; // tilføj 1 cookie
+
+        updateScore(newScore); // opdaterer antal cookies
+    }, 1000) // 1000ms = 1 sekund
 }
 
-autoClick.addEventListener("click", () => {
-    const price = autoClick.getAttribute("data-price");
-    const score = document.querySelector("#score span");
-    const scoreValue = parseInt(score.innerText)
+// autoClickCookie(); // opretter en cookie particle
+autoClick.addEventListener("click", () => { // tilføjer eventlistener til auto-click
+    const price = autoClick.getAttribute("data-price"); // henter prisen på auto-clicks
+    const score = document.querySelector("#score span"); // henter antal cookies fra html
+    const scoreValue = parseInt(score.innerText) // henter antal cookies fra html
 
-    if (scoreValue >= price) {
-        updatePowerupsStorage("auto-click");
+    if (scoreValue >= price) { // hvis der er nok cookies
+        updatePowerupsStorage("auto-click"); // opdaterer antal powerups
 
-        const storage = getStorage();
-        const quantAutoClicks = storage.powerups.filter(powerup => powerup == "auto-click").length;
+        const storage = getStorage(); // henter antal cookies og powerups fra localstorage
+        const quantAutoClicks = storage.powerups.filter(powerup => powerup == "auto-click").length; // henter antal auto-clicks
 
-        const newScore = scoreValue - price;
+        const newScore = scoreValue - price; // trækker prisen fra antal cookies
 
-        updateScore(newScore)
+        updateScore(newScore) // opdaterer antal cookies
 
-        if(quantAutoClicks == 1) {
-            autoClick.setAttribute("data-price", 100 * 2);
-            autoClickTextPrice.innerHTML = 100 * 2;
-        } else {
-            autoClick.setAttribute("data-price", 100 * (quantAutoClicks + 1));
-            autoClickTextPrice.innerHTML = 100 * (quantAutoClicks + 1);
+        if(quantAutoClicks == 1) { // hvis der er 1 auto-click
+            autoClick.setAttribute("data-price", 100 * 2); // sætter prisen til 100 * 2
+            autoClickTextPrice.innerHTML = 100 * 2; // opdaterer prisen på siden
+        } else { // hvis der er flere auto-clicks
+            autoClick.setAttribute("data-price", 100 * (quantAutoClicks + 1)); // sætter prisen til 100 * (quantAutoClicks + 1)
+            autoClickTextPrice.innerHTML = 100 * (quantAutoClicks + 1); // opdaterer prisen på siden
         }
 
-        document.querySelector(".auto-clicks").classList.remove("disable");
+        document.querySelector(".auto-clicks").classList.remove("disable"); // fjerner disable class fra auto-clicks
 
-        document.querySelector(".auto-clicks .cursors").innerHTML += '<img src="img/cursor.png" alt="cursor" id="cursor" class="cursor auto">'
+        document.querySelector(".auto-clicks .cursors").innerHTML += '<img src="img/cursor.png" alt="cursor" id="cursor" class="cursor auto">' // tilføjer en cursor til auto-clicks
 
-        autoClickCookie();
-    } else {
-        autoClick.classList.add("fejl")
-        setTimeout(() => {
-            autoClick.classList.remove("fejl")
-        }, 300);
+        autoClickCookie(); // opretter en cookie particle
+    } else { // hvis der ikke er nok cookies
+        autoClick.classList.add("fejl") // tilføjer fejl class til auto-click
+        setTimeout(() => { // fjerner fejl class fra auto-click
+            autoClick.classList.remove("fejl") // fjerner fejl class fra auto-click
+        }, 300); // 300ms = 0.3 sekunder
     }
 })
 
-upgradeClick.addEventListener("click", () => {
-    const price = upgradeClick.getAttribute("data-price");
-    const score = document.querySelector("#score span");
-    const scoreValue = parseInt(score.innerText)
+// Path: app.js 
+upgradeClick.addEventListener("click", () => { // tilføjer eventlistener til upgrade-click
+    const price = upgradeClick.getAttribute("data-price"); // henter prisen på upgrade-clicks
+    const score = document.querySelector("#score span"); // henter antal cookies fra html
+    const scoreValue = parseInt(score.innerText) // henter antal cookies fra html
 
-    if (scoreValue >= price) {
-        updatePowerupsStorage("upgrade-click");
+    if (scoreValue >= price) { // hvis der er nok cookies
+        updatePowerupsStorage("upgrade-click"); // opdaterer antal powerups
 
-        const storage = getStorage();
-        const multiplier = storage.powerups.filter(powerup => powerup == "upgrade-click").length;
+        const storage = getStorage(); // henter antal cookies og powerups fra localstorage
+        const multiplier = storage.powerups.filter(powerup => powerup == "upgrade-click").length; // henter antal upgrade-clicks
 
-        const newScore = scoreValue - price;
+        const newScore = scoreValue - price;    // trækker prisen fra antal cookies
 
-        updateScore(newScore)
+        updateScore(newScore)   // opdaterer antal cookies
 
-        if(multiplier == 1) {
-            upgradeClick.setAttribute("data-price", 100 * 2);
-            upgradeClickTextPrice.innerHTML = 100 * 2;
+        if(multiplier == 1) {  // hvis der er 1 upgrade-click
+            upgradeClick.setAttribute("data-price", 100 * 2); // sætter prisen til 100 * 2
+            upgradeClickTextPrice.innerHTML = 100 * 2; // opdaterer prisen på siden
         } else {
-            upgradeClick.setAttribute("data-price", 100 * (2 ** multiplier));
-            upgradeClickTextPrice.innerHTML = 10 * (2 ** multiplier);
+            upgradeClick.setAttribute("data-price", 100 * (2 ** multiplier)); // sætter prisen til 100 * (2 ^ multiplier)
+            upgradeClickTextPrice.innerHTML = 100 * (2 ** multiplier);  // opdaterer prisen på siden
         }
-    } else {
-        upgradeClick.classList.add("fejl")
-        setTimeout(() => {
-            upgradeClick.classList.remove("fejl")
-        }, 300);
+    } else { // hvis der ikke er nok cookies
+        upgradeClick.classList.add("fejl") // tilføjer fejl class til upgrade-click
+        setTimeout(() => { // fjerner fejl class fra upgrade-click
+            upgradeClick.classList.remove("fejl") // fjerner fejl class fra upgrade-click
+        }, 300); // 300ms = 0.3 sekunder
     }
 })
 
+// Path: app.js
+const getSavedData = () => { /* henter antal cookies og powerups fra localstorage */
+    const storage = getStorage(); // henter antal cookies og powerups fra localstorage
 
-    if (storage.powerups.includes("upgrade-click")) {
-        const multiplier = storage.powerups.filter(powerup => powerup == "upgrade-click").length;
+    updateScore(storage.cookies); // opdaterer antal cookies
 
-        if(multiplier == 1) {
-            upgradeClick.setAttribute("data-price", 10 * 2);
-            upgradeClickTextPrice.innerHTML = 10 * 2;
-        } else {
-            upgradeClick.setAttribute("data-price", 10 * (2 ** multiplier));
-            upgradeClickTextPrice.innerHTML = 10 * (2 ** multiplier);
+    if (storage.powerups.includes("upgrade-click")) { // hvis der er en upgrade-click powerup
+        const multiplier = storage.powerups.filter(powerup => powerup == "upgrade-click").length; // henter antal upgrade-clicks
+
+        if(multiplier == 1) {   // hvis der er 1 upgrade-click
+            upgradeClick.setAttribute("data-price", 100 * 2); // sætter prisen til 100 * 2
+            upgradeClickTextPrice.innerHTML = 100 * 2; // opdaterer prisen på siden
+        } else {   // hvis der er flere upgrade-clicks
+            upgradeClick.setAttribute("data-price", 100 * (2 ** multiplier)); // sætter prisen til 100 * (2 ^ multiplier)
+            upgradeClickTextPrice.innerHTML = 100 * (2 ** multiplier); // opdaterer prisen på siden
         }
     }
 
-    if(storage.powerups.includes("auto-click")) {
-        const quantAutoClicks = storage.powerups.filter(powerup => powerup == "auto-click").length;
+    if(storage.powerups.includes("auto-click")) { // hvis der er en auto-click powerup
+        const quantAutoClicks = storage.powerups.filter(powerup => powerup == "auto-click").length; // henter antal auto-clicks
 
-        document.querySelector(".auto-clicks").classList.remove("disable")
+        document.querySelector(".auto-clicks").classList.remove("disable") // fjerner disable class fra auto-clicks
 
-        if(quantAutoClicks == 1) {
-            autoClick.setAttribute("data-price", 10 * 2);
-            autoClickTextPrice.innerHTML = 10 * 2;
+        if(quantAutoClicks == 1) { // hvis der er 1 auto-click
+            autoClick.setAttribute("data-price", 100 * 2); // sætter prisen til 100 * 2
+            autoClickTextPrice.innerHTML = 100 * 2; // opdaterer prisen på siden
         } else {
-            autoClick.setAttribute("data-price", 10 * (quantAutoClicks + 1));
-            autoClickTextPrice.innerHTML = 10 * (quantAutoClicks + 1);
+            autoClick.setAttribute("data-price", 100 * (quantAutoClicks + 1)); // sætter prisen til 100 * (quantAutoClicks + 1)
+            autoClickTextPrice.innerHTML = 100 * (quantAutoClicks + 1); // opdaterer prisen på siden
         }
 
-        for (i=1;i <= quantAutoClicks; i++) {
-            autoClickCookie();
+        for (i=1;i <= quantAutoClicks; i++) { // opretter en cookie particle for hvert antal auto-clicks
+            autoClickCookie(); // opretter en cookie particle
 
-            document.querySelector(".auto-clicks").classList.remove("disable");
-            document.querySelector(".auto-clicks .cursors").innerHTML += '<img src="img/cursor.png" alt="cursor" id="cursor" class="cursor auto">'
+            document.querySelector(".auto-clicks").classList.remove("disable"); // fjerner disable class fra auto-clicks
+            document.querySelector(".auto-clicks .cursors").innerHTML += '<img src="img/cursor.png" alt="cursor" id="cursor" class="cursor auto"> ' // tilføjer en cursor til auto-clicks
 
         }
     }
+}
+
+// Gemmer data når siden lukkes eller genindlæses (f5) 
+document.addEventListener("load", getSavedData()); // henter antal cookies og powerups fra localstorage
---
+++ tutorial.md
@@ -0,0 +1,16 @@
+[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)]
+(https://classroom.github.com/online_ide?assignment_repo_id=12244829&assignment_repo_type=AssignmentRepo)
+
+
+
+Cookie Clicker er et simpelt spil, hvor spilleren skal klikke på en cookie for at tjene point. Jo flere point spilleren tjener, jo flere cookies kan spilleren købe, som automatisk genererer point. Spillet er kendt for sin afhængighedsskabende natur og simple gameplay.
+
+Her er en guide til at spille Cookie Clicker:
+
+Åbn spillet i din browser.
+Klik på cookien for at tjene point.
+Brug pointene til at købe cookies, som automatisk genererer point.
+Brug pointene til at købe opgraderinger, som øger pointgenereringen.
+Gentag trin 2-4 for at tjene flere point og købe flere cookies og opgraderinger.
+Hold øje med specielle begivenheder og præmier, som kan hjælpe dig med at tjene flere point.
+Fortsæt med at spille og se, hvor mange point du kan tjene!
